fd1f99f745ee91e86f7c1b40334a70cc3d40ed1c
Unfaithh <99218648+Unfaithh@users.noreply.github.com>
2022-02-07 20:49:21 -0300

    La mama del yamir 2.0

--- index.php
+++ index.php
@@ -4,24 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
-        <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
-        <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
-        <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
-        <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
-        <title>Yamir no te quedi mirando no mas, metele mano</title>
-    <title><b>La mamá de la mamá de la mamá
-       </title>
+    <title>Prueba de Pagina Web</title>
 </head>
 <body>
     Estoy cambiando la wea
